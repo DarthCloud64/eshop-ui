b1113c9568d718b889da50a495028b7026e63d66
Robert Reyes <neorob22@gmail.com>
2025-03-22 12:11:56 -0500

    added auth0

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import CartIcon from './carts/cart-icon.component'
 import Products from './products/products.component'
 import ProductDetails from './products/product-details.component'
 import Cart from './models/cart'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Link as RouterLink } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Grid2, Link } from '@mui/material'
 
 const App = () => {
   const [cart, setCart] = useState<Cart>(() => {
@@ -17,10 +19,37 @@ const App = () => {
     }
 });
 
+  const {loginWithRedirect, logout, isAuthenticated, user} = useAuth0();
+  
+  if(!isAuthenticated){
+      return  (
+        <>
+          <button onClick={() => loginWithRedirect()}>Login</button>
+        </>
+    )
+  }
+
   return (
     <>
-      <CartIcon cart={cart}/>
       <BrowserRouter>
+        <Grid2 container spacing={2}>
+        <Grid2 size={2}>
+            <button onClick={() => logout()}>Logout</button>
+          </Grid2>
+          <Grid2 size={2}>
+            <h3>{user?.name}</h3>
+          </Grid2>
+          <Grid2 size={2}>
+            <h3>{user?.email}</h3>
+          </Grid2>
+          <Grid2 size={2}>
+            <CartIcon cart={cart}/>
+          </Grid2>
+          <Grid2 size={2}>
+            <Link component={RouterLink} to="/products">Products</Link>
+          </Grid2>
+        </Grid2>
+
         <Routes>
           <Route path="/products" element={<Products/>}/>
           <Route path="/products/:productId" element={<ProductDetails cart={cart} setCart={setCart}/>}/>
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const auth0Client = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 const darkTheme = createTheme({
   palette: {
@@ -12,9 +16,11 @@ const darkTheme = createTheme({
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline enableColorScheme/>
-      <App/>
-    </ThemeProvider>
+    <Auth0Provider domain={auth0Domain} clientId={auth0Client} authorizationParams={{redirect_uri: window.location.origin}} cacheLocation="localstorage">
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline enableColorScheme/>
+        <App/>
+      </ThemeProvider>
+    </Auth0Provider>
   </StrictMode>,
 )
